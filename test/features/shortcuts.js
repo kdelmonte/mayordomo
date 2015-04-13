83082afee8016adfb45ccb37141ebd67c0e1bab9
Kelvin Del Monte <kdelmonten@gmail.com>
2015-04-13 06:16:21 -0400

    Update label on test

--- test/features/shortcuts.js
+++ test/features/shortcuts.js
@@ -1,6 +1,6 @@
 var should = require('should');
 var mayordomo = require('../../mayordomo.js');
-describe('#Wildcards', function(){
+describe('#Shortcuts', function(){
     it('should subscribe and trigger through shortcut method', function(done){
         var domo = mayordomo.new();
         domo.declare('goodbye', true);
